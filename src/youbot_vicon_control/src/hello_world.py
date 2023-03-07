3ec65b432b1ab2feaeb13d40d540cbb12567af33
Anh-Duy Pham <duyanhpham@outlook.com>
2023-03-07 22:48:44 +0100

    added  model

--- src/youbot_vicon_control/src/hello_world.py
+++ src/youbot_vicon_control/src/hello_world.py
@@ -7,6 +7,7 @@ import math
 import time
 
 import sys
+import json
 
 from geometry_msgs.msg import TransformStamped
 from geometry_msgs.msg import Twist
@@ -21,6 +22,22 @@ transform_torso_world=almath.Transform()
 transform_head_world=almath.Transform()
 transform_arm_world=almath.Transform()
 transform_wand_world=almath.Transform()
+
+################# DNN Helpers ############################
+def sigmoid(x):
+	return 1.0 / (1.0 + np.exp(-x))
+
+def SiLU(x):
+	return x*sigmoid(x)
+
+def softmax(x):
+	s = np.max(x, axis=1)
+	s = s[:, np.newaxis]
+	e_x = np.exp(x - s)
+	div = np.sum(e_x, axis=1)
+	div = div[:, np.newaxis] # dito
+	return e_x / div
+##########################################################
 
 def torso_callback(msg):
 	global pose_torso_world
@@ -154,7 +171,7 @@ def read_pose_2():
 		print(position6D)
 		rate.sleep()
 		
-def read_pose_3():
+def read_pose_3(model):
 	global transform_wand_world
 	rospy.init_node('hello_world',anonymous=False)
 	
@@ -167,6 +184,11 @@ def read_pose_3():
 	#transform_head_torso=transform_head_world*transform_torso_world.inverse()
 	#position6D=almath.position6DFromTransform(transform_head_torso)
 	vel=Twist()
+	dataset = []
+	count = 0
+	gap = 0
+	last_det = 5
+	last_cls = 5
 	while not rospy.is_shutdown():
 		position6D=almath.position6DFromTransform(transform_wand_world)
 		vel.linear.x=-(input_limit(position6D.wx,1.0))*0.6
@@ -175,13 +197,87 @@ def read_pose_3():
 		vel.angular.x=0
 		vel.angular.y=0
 		vel.angular.z=(input_limit(position6D.wz,1.0))*0.4
+		data = [vel.angular.x, vel.angular.y, vel.angular.z, vel.angular.w, vel.linear.x, vel.linear.y, vel.linear.z]
+		dataset = arrange_timeseries(dataset, data)
+		if len(dataset) == 131:
+			out = detect_gesture(model, dataset, count, gap, last_det, last_cls)
+			if len(out) == 4:
+				# last_det is the prediction, in which 0: BF, 1: FB, 2: CCW, 3: CW
+				last_cls, last_det, count, gap = out
+			else:
+				last_cls, count, gap = out
 		pub.publish(vel)
 		print(position6D)
 		rate.sleep()
+
+def load_model(link_to_file):
+	with open(link_to_file, "r") as f:
+		model = json.load(f)
+
+	for key in list(model.keys()):
+		model[key]["w"] = np.array(model[key]["w"])
+		model[key]["b"] = np.array(model[key]["b"])
+
+	return model
+
+def arrange_timeseries(dataset, data):
+	if len(dataset) < 131:
+		dataset.append(data)
+	else:
+		dataset.pop(0)
+		dataset.append(data)
+
+	return dataset
+
+def detect_gesture(model, dataset, count, gap, last_det):
+	dataset[:,4:] = dataset[:,4:] / 4000
+	for l in range(3):
+		if l < 2:
+			x = SiLU(np.array(dataset).reshape(-1,1).dot(model["layer_"+str(l)]["w"].T)+model["layer_"+str(l)]["b"])
+		else:
+			x = softmax(np.array(dataset).reshape(-1,1).dot(model["layer_"+str(l)]["w"].T)+model["layer_"+str(l)]["b"])
+
+	# Extract class
+	cls = np.argmax(x)
+
+	# Measure changes over a window of 5 time frames
+	diff0 = np.abs(np.sum(np.array(dataset)[-1,0] - np.array(dataset)[-6:-1, 0].mean()))
+	diff1 = np.abs(np.sum(np.array(dataset)[-1,1] - np.array(dataset)[-6:-1, 1].mean()))
+	diff2 = np.abs(np.sum(np.array(dataset)[-1,2] - np.array(dataset)[-6:-1, 2].mean()))
+	diff3 = np.abs(np.sum(np.array(dataset)[-1,3] - np.array(dataset)[-6:-1, 3].mean()))
+	diff4 = np.abs(np.sum(np.array(dataset)[-1,4] - np.array(dataset)[-6:-1, 4].mean()))
+	diff5 = np.abs(np.sum(np.array(dataset)[-1,5] - np.array(dataset)[-6:-1, 5].mean()))
+	diff6 = np.abs(np.sum(np.array(dataset)[-1,6] - np.array(dataset)[-6:-1, 6].mean()))
+
+	# Measure total change
+	diff_all = np.sum((diff0,diff1,diff2,diff3,diff4,diff5,diff6))
+
+	# Decide a gesture to belong to one class if it: matches the last detection, confidence score > 0.9 and has changes in range (1,4)
+	if cls == last_cls and x.max() > 0.9 and (diff_all > 1 and diff_all < 4):
+		count += 1
+	else:
+		count = 0
+
+	# Gap between 2 detected gesture
+	gap += 1
+
+	# Condition to output the gesture
+	if cls != last_det and gap > 131 and count >= 5:
+		gap = 0
+		count = 0
+		last_det = cls
+		last_cls = cls
+		return last_cls, last_det, count, gap
+
+	last_cls = cls
+
+	return last_cls, count, gap
+
 	
 if __name__ == '__main__':
 	try:
-		read_pose_3()
+		model = load_model("../model.json")
+		read_pose_3(model)
 		
 	except rospy.ROSInterruptException:
 		pass
